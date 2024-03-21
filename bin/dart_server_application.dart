0c8abb21a8c25f8fb193564781d8cb437136b7df
Andy <lili2011050508@163.com>
2024-03-21 21:59:49 +0800

    update

--- bin/dart_server_application.dart
+++ bin/dart_server_application.dart
@@ -1,6 +1,6 @@
 import 'package:dart_server_application/web_server.dart' as server;
 
 void main(List<String> arguments) {
-  final bindIp = arguments.length > 1 ? arguments.first : null;
+  final bindIp = arguments.isNotEmpty ? arguments.first : null;
   server.runSever(bindIp: bindIp);
 }
